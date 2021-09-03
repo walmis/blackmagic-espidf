76e6847b809197dd3b5d5683201e4f226a5da0a7
Valmantas Paliksa <walmis@gmail.com>
2021-09-03 14:13:08 +0300

    Escape '*' in responses of the Remote Serial Protocol, to avoid that it is interpreted as the start of an RLE sequence

--- main/gdb_packet.cpp
+++ main/gdb_packet.cpp
@@ -169,7 +169,7 @@ void GDB::gdb_putpacket(const char *packet, int size, char pktstart)
 			else
 				DEBUG_GDB_WIRE("\\x%02X", c);
 #endif
-			if((c == '$') || (c == '#') || (c == '}')) {
+			if((c == '$') || (c == '#') || (c == '}') || (c == '*')) {
 				gdb_if_putchar('}', 0);
 				gdb_if_putchar(c ^ 0x20, 0);
 				csum += '}' + (c ^ 0x20);
