17f20bf4bbff8d3723190182b74f5f0ad4fa7816
Sean Cross <sean@xobs.io>
2021-11-16 12:34:50 +0800

    platform: fix wifi joining
    Signed-off-by: Sean Cross <sean@xobs.io>

--- main/platform.c
+++ main/platform.c
@@ -445,6 +445,9 @@ void wifi_init_softap()
 #if CONFIG_ESP_WIFI_IS_STATION
 static esp_err_t wifi_fill_sta_config(wifi_config_t *wifi_config) {
     ESP_LOGI(__func__, "wifi_fill_sta_config begun.");
+
+    memset(wifi_config, 0, sizeof(*wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
     while (1) {
         uint16_t i;
         wifi_scan_config_t scan_config = {0};
@@ -456,24 +459,32 @@ static esp_err_t wifi_fill_sta_config(wifi_config_t *wifi_config) {
 
         for (i = 0; i < num_aps; i++) {
             if ((strlen(CONFIG_ESP_WIFI_SSID) > 0) && !strcmp((char *)scan_results[i].ssid, CONFIG_ESP_WIFI_SSID)) {
+	            // DEBUG_WARN("Connecting to %s\n", CONFIG_ESP_WIFI_SSID);
                 strncpy((char *)wifi_config->sta.ssid, CONFIG_ESP_WIFI_SSID, sizeof(wifi_config->sta.ssid));
                 strncpy((char *)wifi_config->sta.password, CONFIG_ESP_WIFI_PASSWORD, sizeof(wifi_config->sta.password));
-                return;
+                ESP_ERROR_CHECK(esp_wifi_stop());
+                return ESP_OK;
             }
             if ((strlen(CONFIG_ESP_WIFI_SSID2) > 0) && !strcmp((char *)scan_results[i].ssid, CONFIG_ESP_WIFI_SSID2)) {
                 strncpy((char *)wifi_config->sta.ssid, CONFIG_ESP_WIFI_SSID2, sizeof(wifi_config->sta.ssid));
                 strncpy((char *)wifi_config->sta.password, CONFIG_ESP_WIFI_PASSWORD2, sizeof(wifi_config->sta.password));
-                return;
+	            // DEBUG_WARN("Connecting to %s (password: %s)\n", wifi_config->sta.ssid, wifi_config->sta.password);
+                ESP_ERROR_CHECK(esp_wifi_stop());
+                return ESP_OK;
             }
             if ((strlen(CONFIG_ESP_WIFI_SSID3) > 0) && !strcmp((char *)scan_results[i].ssid, CONFIG_ESP_WIFI_SSID3)) {
+	            // DEBUG_WARN("Connecting to %s\n", CONFIG_ESP_WIFI_SSID3);
                 strncpy((char *)wifi_config->sta.ssid, CONFIG_ESP_WIFI_SSID3, sizeof(wifi_config->sta.ssid));
                 strncpy((char *)wifi_config->sta.password, CONFIG_ESP_WIFI_PASSWORD3, sizeof(wifi_config->sta.password));
-                return;
+                ESP_ERROR_CHECK(esp_wifi_stop());
+                return ESP_OK;
             }
             if ((strlen(CONFIG_ESP_WIFI_SSID4) > 0) && !strcmp((char *)scan_results[i].ssid, CONFIG_ESP_WIFI_SSID4)) {
+	            // DEBUG_WARN("Connecting to %s\n", CONFIG_ESP_WIFI_SSID4);
                 strncpy((char *)wifi_config->sta.ssid, CONFIG_ESP_WIFI_SSID4, sizeof(wifi_config->sta.ssid));
                 strncpy((char *)wifi_config->sta.password, CONFIG_ESP_WIFI_PASSWORD4, sizeof(wifi_config->sta.password));
-                return;
+                ESP_ERROR_CHECK(esp_wifi_stop());
+                return ESP_OK;
             }
         }
     }
@@ -506,9 +517,19 @@ void wifi_init_sta()
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     wifi_fill_sta_config(&wifi_config);
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
 
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    result = esp_base_mac_addr_get(mac_address);
+    if(result == ESP_ERR_INVALID_MAC) {
+      ESP_LOGE(__func__, "base mac address invalid.  reading from fuse.");
+      ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac_address));
+      ESP_ERROR_CHECK(esp_base_mac_addr_set(mac_address));
+      ESP_LOGE(__func__, "base mac address configured.");
+    }
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(__func__, "wifi_init_sta finished.");
 
