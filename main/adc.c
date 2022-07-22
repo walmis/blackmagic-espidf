7e02450d056746f4cac2c50f9233f27ed51bb265
Sean Cross <sean@xobs.io>
2022-07-22 15:28:24 +0800

    adc: use new cali api
    The API has changed in the past week. Use this new API.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- main/adc.c
+++ main/adc.c
@@ -1,72 +1,99 @@
-#include "esp_adc_cal.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 #include "esp_log.h"
-#include "driver/adc.h"
 
 #define TAG "bmp-adc"
 
-// ADC Calibration
-#if CONFIG_IDF_TARGET_ESP32
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_VREF
-#elif CONFIG_IDF_TARGET_ESP32S2
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP
-#elif CONFIG_IDF_TARGET_ESP32C3
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP
-#elif CONFIG_IDF_TARGET_ESP32S3
-#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP_FIT
+#define ADC_CHANNEL ADC_CHANNEL_7
+#define ADC_UNIT    ADC_UNIT_1
+
+static bool adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+	adc_cali_handle_t handle = NULL;
+	esp_err_t ret = ESP_FAIL;
+	bool calibrated = false;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+	if (!calibrated) {
+		ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+		adc_cali_curve_fitting_config_t cali_config = {
+			.unit_id = unit,
+			.atten = atten,
+			.bitwidth = ADC_BITWIDTH_DEFAULT,
+		};
+		ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+		if (ret == ESP_OK) {
+			calibrated = true;
+		}
+	}
 #endif
 
-#define ADC_CHANNEL ADC1_CHANNEL_7
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+	if (!calibrated) {
+		ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+		adc_cali_line_fitting_config_t cali_config = {
+			.unit_id = unit,
+			.atten = atten,
+			.bitwidth = ADC_BITWIDTH_DEFAULT,
+		};
+		ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+		if (ret == ESP_OK) {
+			calibrated = true;
+		}
+	}
+#endif
 
-static bool adc_calibration_init(esp_adc_cal_characteristics_t *adc_characteristics)
-{
-	esp_err_t ret;
-	bool cali_enable = false;
-
-	ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
-	if (ret == ESP_ERR_NOT_SUPPORTED) {
-		ESP_LOGW(TAG, "Calibration scheme not supported, skip software calibration");
-	} else if (ret == ESP_ERR_INVALID_VERSION) {
+	*out_handle = handle;
+	if (ret == ESP_OK) {
+		ESP_LOGI(TAG, "Calibration Success");
+	} else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
 		ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-	} else if (ret == ESP_OK) {
-		cali_enable = true;
-		esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_2_5, ADC_WIDTH_BIT_DEFAULT, 0, adc_characteristics);
 	} else {
-		ESP_LOGE(TAG, "Invalid arg");
+		ESP_LOGE(TAG, "Invalid arg or no memory");
 	}
 
-	return cali_enable;
+	return calibrated;
 }
 
-static bool adc_init(esp_adc_cal_characteristics_t *adc_characteristics)
+static bool adc_init(adc_cali_handle_t *adc_cali_handle, adc_oneshot_unit_handle_t *adc_handle)
 {
-	bool ret = adc_calibration_init(adc_characteristics);
-	ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
-	ESP_ERROR_CHECK(adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_2_5));
-	return ret;
+	const adc_oneshot_unit_init_cfg_t init_config = {
+		.unit_id = ADC_UNIT,
+	};
+	const adc_oneshot_chan_cfg_t config = {
+		.bitwidth = ADC_BITWIDTH_DEFAULT,
+		.atten = ADC_ATTEN_DB_2_5,
+	};
+	if (!adc_calibration_init(ADC_UNIT, ADC_ATTEN_DB_2_5, adc_cali_handle)) {
+		return false;
+	}
+	ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, adc_handle));
+	ESP_ERROR_CHECK(adc_oneshot_config_channel(*adc_handle, ADC_CHANNEL, &config));
+	return true;
 }
 
 int32_t adc_read_system_voltage(void)
 {
-	static bool calibrated = false;
-	static esp_adc_cal_characteristics_t adc_characteristics;
+	static adc_cali_handle_t adc_cali_handle;
+	static adc_oneshot_unit_handle_t adc_handle;
 	int adc_reading;
+	int voltage_reading;
 
-	if (!calibrated) {
-		calibrated = adc_init(&adc_characteristics);
-	}
-	if (!calibrated) {
+	if (adc_handle == NULL && !adc_init(&adc_cali_handle, &adc_handle)) {
 		return -1;
 	}
 
-	adc_reading = adc1_get_raw(ADC_CHANNEL);
-	ESP_LOGD(TAG, "raw  data: %d", adc_reading);
+	ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL, &adc_reading));
+	ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT + 1, ADC_CHANNEL, adc_reading);
+	// ESP_LOGD(TAG, "raw  data: %d", adc_reading);
 
-	uint32_t voltage_reading = esp_adc_cal_raw_to_voltage(adc_reading, &adc_characteristics);
+	ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc_cali_handle, adc_reading, &voltage_reading));
+	ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT + 1, ADC_CHANNEL, voltage_reading);
 
-    // Farpatch has a divider that's 82k on top and 20k on the bottom.
-    uint32_t adjusted_voltage = (voltage_reading * 51) / 10;
-
+	// Farpatch has a divider that's 82k on top and 20k on the bottom.
+	int adjusted_voltage = (voltage_reading * 51) / 10;
 	ESP_LOGD(TAG, "cal data: %d mV, adjusted: %d mV", voltage_reading, adjusted_voltage);
 
-    return adjusted_voltage;
+	return adjusted_voltage;
 }
