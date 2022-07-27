5b84027458517265199394521f7113637110f9f8
Sean Cross <sean@xobs.io>
2022-07-27 09:47:54 +0800

    gdb_if: make GDB process run at IDLE priority
    By running the GDB process at IDLE, it allows the IDLE thread to get
    time, which allows the watchdog to be run.
    
    Without this, the system reboots after a few seconds when doing
    SWD-heavy operations such as RTT.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- main/gdb_if.c
+++ main/gdb_if.c
@@ -219,7 +219,10 @@ static void new_gdb_wifi_instance(int sock)
 	memset(instance, 0, sizeof(*instance));
 	instance->sock = sock;
 
-	xTaskCreate(gdb_wifi_task, name, 5500, (void *)instance, 1, &instance->pid);
+	// Create the task at the IDLE priority, to allow the idle thread to keep
+	// the watchdog happy. This will get preemptively scheduled at the same
+	// priority as the IDLE task.
+	xTaskCreate(gdb_wifi_task, name, 5500, (void *)instance, tskIDLE_PRIORITY, &instance->pid);
 }
 
 void gdb_net_task(void *arg)
